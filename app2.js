7d9d8b8f7b14ea25aeb52019f90fb5df7eea8e7f
danielmiranda13 <84930029+danielmiranda13@users.noreply.github.com>
2021-05-27 16:00:18 -0500

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,16 @@
+alert('Hola desde app js!');
+
+console.log( console.log ('Hola Mundo') );
+
+let a = 10;
+let b = 20;
+let c = 'Hola ';
+let d = 'Spiderman';
+let x = a + b;
+
+console.log( c + d);
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+var miNombre = 'Daniel';

94fffa2a44ca137ae323e52feedbb03aa1a6d5f8
danielmiranda13 <84930029+danielmiranda13@users.noreply.github.com>
2021-06-01 15:22:26 -0500

    Add files via upload

---
+++ while-do-while.js
@@ -0,0 +1,27 @@
+
+
+const carros = ['Ford', 'Chevrolet', 'Mazda', 'Honda'];
+
+let i = 0;
+
+while( i < carros.length ) {
+    console.log( carros[i] );
+    // i = i + 1;
+    i++;
+}
+
+
+while( carros [i] ) {
+    if ( i === 1){
+    i++;
+    continue;
+    }
+
+    console.log( carros [i] );
+    i++;
+}
+
+console.warn('Do While');
+do {
+
+} while( );

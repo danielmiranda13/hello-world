aa7697064fb60f9e260882864fe3f193c902ceb1
danielmiranda13 <84930029+danielmiranda13@users.noreply.github.com>
2021-06-01 14:17:51 -0500

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,29 @@
+
+
+const dia = 6;
+
+switch( dia ) {
+    case 0:
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    case 3:
+        console.log('Miércoles');
+        break;
+    case 4:
+        console.log('Jueves');
+        break;
+    case 5:
+        console.log('Viernes');
+        break;
+    case 6:
+        console.log('Sábado');
+        break;
+default: console.log('Tu carta es un 6 de picas?')
+
+   }
